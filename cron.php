bf68abd70a082b9778451841a66b63f67c032731
Alex Manelis <amanelis@gmail.com>
2010-04-21 12:42:30 -0500

    cron update

--- cron.php
+++ cron.php
@@ -1,17 +1,25 @@
 <?PHP
+//define the path to the runing bot implementation
 define('POST_URL', 'http://www.YOURDOMAIN.com/ztbot/run.php');
 
+//we are using curl to run cron.
 $ch = curl_init(POST_URL);
 curl_setopt($ch, CURL_POST, 1);
 curl_setopt($ch, CURL_HEADER, 0);
 curl_setopt($ch, CURL_RETURNTRANSFER, 1);
+
+//exec and store output
 $return_data = curl_exec($ch);
 
+//Starting output just incase you want to see return
 ob_start();
-//echo $return_data."<br />";
 
+//output buffer
+echo $return_data;
+
+//clearn up output buffer
 ob_get_clean();
+
+//close our curl 
 curl_close($ch);
-
-
 ?>
